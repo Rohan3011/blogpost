1f642b130faca292b7848b067381b681808b5b67
rohan3011 <rohankamble3011@gmail.com>
2023-06-09 15:15:06 +0530

    removed unused

--- config/docs.ts
+++ config/docs.ts
@@ -3,10 +3,6 @@ import { DocsConfig } from "types"
 export const docsConfig: DocsConfig = {
   mainNav: [
     {
-      title: "Documentation",
-      href: "/docs",
-    },
-    {
       title: "Guides",
       href: "/guides",
     },
@@ -18,37 +14,6 @@ export const docsConfig: DocsConfig = {
         {
           title: "Introduction",
           href: "/docs",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/documentation",
-        },
-        {
-          title: "Contentlayer",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Components",
-          href: "/docs/documentation/components",
-        },
-        {
-          title: "Code Blocks",
-          href: "/docs/documentation/code-blocks",
-        },
-        {
-          title: "Style Guide",
-          href: "/docs/documentation/style-guide",
-        },
-        {
-          title: "Search",
-          href: "/docs/in-progress",
-          disabled: true,
         },
       ],
     },
--- config/marketing.ts
+++ config/marketing.ts
@@ -14,9 +14,6 @@ export const marketingConfig: MarketingConfig = {
       title: "Blog",
       href: "/blog",
     },
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
+   
   ],
 }
